f93d2dc87b2023d46b81f6843a91c075eed87779
Raisa Yang <raisa@alittle-town.net>
2015-08-22 23:12:08 -0400

    add navbar

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,22 @@
-<header class="banner" role="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav role="navigation">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
-  </div>
+<header class="banner navbar navbar-default navbar-static-top" role="banner">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target=".navbar-collapse">
+                <span class="sr-only"><?= __('Toggle navigation', 'sage'); ?></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <h4 class="navbar-brand"><a href="<?= esc_url(home_url('/')); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/tastebookoriginal.png" alt="<?php bloginfo('name'); ?>" width="180" height="auto"></a></h4>
+        </div>
+
+        <nav class="collapse navbar-collapse" role="navigation">
+            <?php
+            if (has_nav_menu('primary_navigation')) :
+                wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav navbar-right']);
+            endif;
+            ?>
+        </nav>
+    </div>
 </header>
