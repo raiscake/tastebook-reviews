487c22bb615d13f76a3a8b496fc36930f4b29a07
Raisa Yang <raisa@alittle-town.net>
2015-08-29 22:13:18 -0400

    add google fonts

--- templates/content-front-page.php
+++ templates/content-front-page.php
@@ -1,6 +1,9 @@
 <div class="col-sm-6">
     <article <?php post_class(); ?>>
-        <header>
+        <header class="magazine-title">
+            <?php if (has_post_thumbnail()) :
+                the_post_thumbnail('frontpage-img');
+            endif; ?>
             <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
             <?php get_template_part('templates/entry-meta'); ?>
         </header>
