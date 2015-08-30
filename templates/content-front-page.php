1ab783afeab30a913b592c6b45717a213e0f171d
Raisa Yang <raisa@alittle-town.net>
2015-08-30 18:48:59 -0400

    style titles

--- templates/content-front-page.php
+++ templates/content-front-page.php
@@ -1,11 +1,14 @@
 <div class="col-md-6">
     <article <?php post_class('magazine-entry'); ?>>
         <header class="magazine-title">
-            <?php if (has_post_thumbnail()) :
-                the_post_thumbnail('frontpage-img');
-            endif; ?>
-            <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-        </header>
+            <div class="aspect-ratio">
+                <div class="entry-overlay"></div>
+                <?php if (has_post_thumbnail()) :
+                    the_post_thumbnail('frontpage-img');
+                endif; ?>
+                <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            </div>
+                   </header>
         <div class="entry-summary">
             <?php the_excerpt(); ?>
         </div>
