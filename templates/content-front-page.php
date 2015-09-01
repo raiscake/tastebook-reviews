dbf0b8a21684515f7c5db336c28a7acf94b4ef2f
Raisa Yang <raisa@alittle-town.net>
2015-09-01 00:28:57 -0400

    add fb share link

--- templates/content-front-page.php
+++ templates/content-front-page.php
@@ -14,10 +14,17 @@
         </div>
         <div class="entry-meta small">
             <time class="updated" datetime="<?= get_post_time('c', true); ?>"><?= get_the_date(); ?></time>
-            <span class="comments-count pull-right">
-                <a class="twitter popup" href="http://twitter.com/intent/tweet?text=<?php the_title(); ?> <?php the_permalink(); ?>"><i class="fa fa-twitter"></i></a>
-                <?php comments_popup_link(  '<i class="fa fa-comments"></i> 0' , '<i class="fa fa-comments"></i> 1', '<i class="fa fa-comments"></i> %' ); ?>
-            </span>
+            <ul class="comments-count list-inline pull-right">
+                <li>
+                    <a target="_blank" href="http://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');return false;"><i class="fa fa-facebook"></i></a>
+                </li>
+                <li>
+                    <a class="twitter popup" href="http://twitter.com/intent/tweet?text=<?php the_title(); ?> <?php the_permalink(); ?>" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');return false;"><i class="fa fa-twitter"></i></a>
+                </li>
+                <li>
+                    <?php comments_popup_link(  '<i class="fa fa-comments"></i> 0' , '<i class="fa fa-comments"></i> 1', '<i class="fa fa-comments"></i> %' ); ?>
+                </li>
+            </ul>
         </div>
     </article>
 </div>
