f24f33761317b2846d5a610a4b7f2b35cc7ee3f2
Raisa Yang <raisa@alittle-town.net>
2015-09-05 01:26:50 -0400

    finish archive template

--- templates/content-page.php
+++ templates/content-page.php
@@ -17,13 +17,18 @@
                 </ul>
 
                 <h3>Posts by Tag</h3>
-                <ul class="by-tags">
-                    <?php echo get_the_tag_list('<li>','</li><li>','</li>'); ?>
-                </ul>
+                    <?php wp_tag_cloud( array(
+                        'format' => 'list',
+                        'smallest'                  => 1,
+                        'largest'                   => 1,
+                        'unit'                      => 'em',
+                    ) ); ?>
+
             </div>
             <div class="col-sm-6">
                 <h3>Archives by Year</h3>
                 <ul class="by-year">
+                    <?php wp_get_archives(array('type' => 'yearly')); ?>
                 </ul>
                 <h3>Archives by Month</h3>
                 <ul class="by-month">
