98d42403684110fae5abbaa85cfe81df9c492116
Raisa Yang <raisa@alittle-town.net>
2015-09-07 00:47:21 -0400

    style page header

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,5 +1,8 @@
 <?php use Roots\Sage\Titles; ?>
 
 <div class="page-header">
-  <h1><?= Titles\title(); ?></h1>
+    <div class="page-title-wrap">
+        <div class="line-bar"></div>
+        <h1 class="page-title""><?= Titles\title(); ?></h1>
+    </div>
 </div>
