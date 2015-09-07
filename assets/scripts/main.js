a7b8de3bd5345de40c03e6853a338ac8e0ba70e6
Raisa Yang <raisa@alittle-town.net>
2015-09-07 01:01:49 -0400

    qa

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -19,6 +19,13 @@
     'common': {
       init: function() {
         // JavaScript to be fired on all pages
+          var featuredLink = $('.featured-link');
+          featuredLink.mouseenter(function(){
+              featuredLink.closest('overlay').addClass('highlight');
+          });
+          featuredLink.mouseleave(function(){
+              featuredLink.closest('overlay').removeClass('highlight');
+          })
       },
       finalize: function() {
         // JavaScript to be fired on all pages, after page specific JS is fired
