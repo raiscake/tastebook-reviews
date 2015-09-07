9b30b9333ca7d68da36574789e595a3907716924
Raisa Yang <raisa@alittle-town.net>
2015-09-07 00:11:00 -0400

    add footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,14 @@
-<footer class="content-info" role="contentinfo">
+<footer class="content-info uppercase" role="contentinfo">
   <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+    <div class="row">
+        <div class="col-sm-12">
+            <ul class="list-unstyled list-inline footer-info">
+                <li>Copyright © <?php echo date("Y"); ?> All Rights Reserved</li>
+                <li><a href="<?php echo home_url(); ?>/feed">Entries (RSS)</a> and <a href="<?php home_url(); ?>/comments/feed/">Comments (RSS)</a></li>
+                <li>Theme by <a href="http://www.little-town.net/" target="_blank">Raisa Y</a></li>
+            </ul>
+            <a href="#top">Back to Top &uarr;</a>
+        </div>
+    </div>
   </div>
 </footer>
