0fb842f186392cf1d649b2024d366533b3e2f3d9
Raisa Yang <raisa@alittle-town.net>
2015-09-07 21:43:46 -0400

    xs responsive

--- lib/extras.php
+++ lib/extras.php
@@ -38,5 +38,5 @@ add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
  */
 add_image_size('carousel-img', 1440, 810, true);
 add_image_size('frontpage-img', 500, 333, true);
-add_image_size('single-img', 1025, 576, true);
+add_image_size('single-img', 1024, 683, true);
 add_image_size('cat-box', 600, 600, true);
