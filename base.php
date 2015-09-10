bc195f8e6d35b5198004b01b0cbf587d346eb5ac
Raisa Yang <raisa@alittle-town.net>
2015-09-10 00:20:30 -0400

    searchbar qa

--- base.php
+++ base.php
@@ -43,5 +43,15 @@ use Roots\Sage\Wrapper;
       wp_footer();
     ?>
   </div>
+
+  <div id="fb-root"></div>
+  <script>(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.4&appId=381458102056878";
+          fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
   </body>
 </html>
