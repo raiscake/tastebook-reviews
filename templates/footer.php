38454aa82514337c46064d9def3449315774e8bf
Raisa Yang <raisa@alittle-town.net>
2015-09-09 23:24:03 -0400

    category box qa

--- templates/footer.php
+++ templates/footer.php
@@ -1,11 +1,11 @@
-<footer class="content-info uppercase" role="contentinfo">
+<footer class="content-info" role="contentinfo">
   <div class="container">
     <div class="row">
         <div class="col-sm-12">
             <ul class="list-unstyled list-inline footer-info">
                 <li>Copyright © <?php echo date("Y"); ?> All Rights Reserved</li>
                 <li><a href="<?php echo home_url(); ?>/feed">Entries (RSS)</a> and <a href="<?php home_url(); ?>/comments/feed/">Comments (RSS)</a></li>
-                <li>Theme by <a href="http://www.little-town.net/" target="_blank">Raisa Y</a></li>
+                <li>Theme by <a href="http://www.raisayang.com/" target="_blank">Raisa Yang</a></li>
             </ul>
             <a href="#top">Back to Top &uarr;</a>
         </div>
