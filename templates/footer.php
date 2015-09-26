167e77850509139f9e8b47596c3e2d31bb2ca80f
Raisa Yang <raisa@alittle-town.net>
2015-09-26 00:09:59 -0400

    add tracker

--- templates/footer.php
+++ templates/footer.php
@@ -6,6 +6,31 @@
                 <li>Copyright © <?php echo date("Y"); ?> All Rights Reserved</li>
                 <li><a href="<?php echo home_url(); ?>/feed">Entries (RSS)</a> and <a href="<?php home_url(); ?>/comments/feed/">Comments (RSS)</a></li>
                 <li>Theme by <a href="http://www.raisayang.com/" target="_blank">Raisa Yang</a></li>
+                <li class="site-tracker">
+                    <script>
+                        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+                        ga('create', 'UA-27650010-2', 'auto');
+                        ga('send', 'pageview');
+
+                    </script>
+                    <script type="text/javascript">
+                        var sc_project=10525053;
+                        var sc_invisible=1;
+                        var sc_security="06de948d";
+                        var scJsHost = (("https:" == document.location.protocol) ?
+                            "https://secure." : "http://www.");
+                        document.write("<sc"+"ript type='text/javascript' src='" + scJsHost+
+                        "statcounter.com/counter/counter.js'></"+"script>");
+                    </script>
+                    <noscript><div class="statcounter"><a title="hit counter"
+                                                          href="http://statcounter.com/free-hit-counter/" target="_blank"><img
+                                    class="statcounter" src="http://c.statcounter.com/10525053/0/06de948d/1/"
+                                    alt="hit counter"></a></div></noscript>
+                </li>
             </ul>
             <a href="#top">Back to Top &uarr;</a>
         </div>
